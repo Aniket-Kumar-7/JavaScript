8652bf9d83dfb16fd443cce6fe4cd7c878fb1063
Aniket Kumar <abcdkumarsai@gmail.com>
2025-05-20 09:34:28 +0000

    learning variable's type

---
+++ Basic_JavaScript/first.js
@@ -0,0 +1,9 @@
+const student_id = 20233052;//used to store for only constant content 
+let name = "Aniket Kumar";
+var college_name = "MNNIT";//not used for undefined scope
+village = "Sai";//not recommended
+let course;
+
+console.table([student_id, village, college_name, name])
+console.log(name);
+console.log(course);
