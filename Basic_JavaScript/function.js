6be1d9754cfae98266d861d2e2c71c7abfe60661
Aniket Kumar <abcdkumarsai@gmail.com>
2025-05-29 06:01:20 +0000

    function implementation

---
+++ Basic_JavaScript/function.js
@@ -0,0 +1,54 @@
+function addTwoNumbers(numberone=3,numbertwo){//default value is set as three if not any other value is given.//otherwise override.
+    if (typeof(numberone)!=Number || typeof(numbertwo)!=Number){
+        numberone=Number(numberone)
+        numbertwo=Number(numbertwo)
+    }
+    return numberone+numbertwo
+}
+let result=addTwoNumbers(5,4)
+// console.log(result)
+
+
+const user ={
+    name:"aniket",
+    age:20
+}
+
+function print(anything){
+    return `user name is ${anything.name} and age is ${anything.age}`
+}   
+// console.log(print(user))
+// or
+// console.log(print({
+//     name:"Aniket",
+//     age:20
+// }))
+
+
+function print_Element(array){
+    return array[2]
+}
+
+const array=[100,100,200]
+console.log(print_Element(array)) 
+
+
+//example of  variable function
+const sum=function(...numbers){
+    let initial_sum=0
+    for(let i=0;i<numbers.length;i++){
+        initial_sum+=numbers[i]
+    }
+    return initial_sum
+}
+
+// console.log(sum(3,4,5))
+
+const person ={
+    name :"Aniket",
+    id:20233052,
+    message:function(message){
+        return `hello! ${this.name} how are you! I hope you'r well.`
+    }
+}
+
